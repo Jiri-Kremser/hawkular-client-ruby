6116e4969a6d470f713828ff5e475b6a19e73053
Jirka Kremser <jkremser@redhat.com>
2016-07-14 13:19:55 +0200

    Convincing Coveralls that it should report once all the rake tasks are done.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
 # This needs to go before all requires to be able to record full coverage
 require 'coveralls'
-Coveralls.wear!
+Coveralls.wear_merged!
+SimpleCov.merge_timeout 1800
 # Now the application requires.
 require 'hawkular/hawkular_client'
 require 'hawkular/hawkular_client_utils'
